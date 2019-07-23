5fe09518b819364fb032b529f940ccfdc133edc3
IbrahimYunuslu <ibrahimyunuslu@gmail.com>
2019-07-24 02:02:43 +0400

    Working copy of Pong game

---
+++ PongProject/Assets/Scripts/ballMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballMovement : MonoBehaviour
+{
+
+    private Rigidbody2D body;
+    private float forceX, forceY;
+    public float posX, posY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        forceX=Random.Range(-5,5);
+        if (forceX == 0)    forceX = 2;
+        forceY=Random.Range(-2,2);
+        if (forceY == 0)    forceY = 2;
+        moveBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posX = transform.position.x;
+        posY = transform.position.y;
+    }
+
+    void moveBall(){
+        body.velocity = new Vector2(forceX, forceY);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.name == "top")
+        {
+            body.velocity += new Vector2(0,1).normalized;
+        }else if (other.gameObject.name == "middle")
+        {
+            body.velocity += new Vector2(1,1).normalized;
+        }else if (other.gameObject.name == "bottom")
+        {
+            body.velocity += new Vector2(0,-1).normalized;
+        }
+    }
+
+}
---
+++ PongProject/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    private Vector3 upMost, downMost;
+    public float speed = 10;
+    private float height;
+    private ballMovement ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GameObject.FindObjectOfType<ballMovement>();
+        height = GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+        downMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0));
+        upMost = Camera.main.ViewportToWorldPoint(new Vector3(1,1,0));
+        downMost.y += height;
+        upMost.y -= height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.tag == "Player")
+        {
+            UserMovement();
+        }
+        else
+        {
+            if (ball.posX < 0)
+            {
+                ComputerMovement();
+            }
+        }
+
+    }
+
+    void UserMovement(){
+        if(Input.GetKey(KeyCode.UpArrow)){
+            MovePlayerUp();
+        }else if(Input.GetKey(KeyCode.DownArrow)){
+            MovePlayerDown();
+        }
+        
+        float restrictedY = Mathf.Clamp(transform.position.y, downMost.y, upMost.y);
+        transform.position = new Vector3(transform.position.x, restrictedY, transform.position.z);
+
+    }
+
+    void ComputerMovement(){
+        if (transform.position.y > ball.posY)
+        {
+            MovePlayerDown();
+        }else if (transform.position.y < ball.posY)
+        {
+            MovePlayerUp();
+        }
+
+        float restrictedY = Mathf.Clamp(transform.position.y, downMost.y, upMost.y);
+        transform.position = new Vector3(transform.position.x, restrictedY, transform.position.z);
+    }
+
+    void MovePlayerUp(){
+        transform.position += Vector3.up * speed * Time.deltaTime;
+    }
+
+    void MovePlayerDown(){
+        transform.position -= Vector3.up * speed * Time.deltaTime;
+    }
+
+}
