2e0d8c7617c53a712f2e7b3adc9c3a9f9330a8df
IbrahimYunuslu <ibrahimyunuslu@gmail.com>
2019-07-25 22:33:08 +0400

    Final game with release build

--- PongProject/Assets/Scripts/ballMovement.cs
+++ PongProject/Assets/Scripts/ballMovement.cs
@@ -10,21 +10,23 @@ public class ballMovement : MonoBehaviour
     [HideInInspector] public float posX, posY, x, y; 
     private scoring score;
     private AudioSource gameAudio;
-    public AudioClip ball, menu, playerHit, computerHit, playerWin, computerWin;
+    public AudioClip ball, playerHit, computerHit;
     private bool toLeft;
     // Start is called before the first frame update
     void Start()
     {
-        gameAudio = GetComponent<AudioSource>();
-        // = GetComponent<AudioClip>();
         x = transform.position.x;
         y = transform.position.y;
+        
+        gameAudio = GetComponent<AudioSource>();
         score = GameObject.FindObjectOfType<scoring>();
         body = GetComponent<Rigidbody2D>();
+        
         forceX=Random.Range(-5,5);
-        if (forceX == 0)    forceX = 2;
+        if (forceX == 0)    forceX = 2f;
         forceY=Random.Range(-2,2);
-        if (forceY == 0)    forceY = 2;
+        if (forceY == 0)    forceY = 2f;
+        
         moveBall();
     }
 
@@ -33,7 +35,12 @@ public class ballMovement : MonoBehaviour
     {
         posX = transform.position.x;
         posY = transform.position.y;
+        
         if (body.velocity.x == 0 && body.velocity.y == 0)
+        {
+            ResetBall();
+        }
+        if (body.velocity.x == 0)
         {
             ResetBall();
         }
@@ -44,16 +51,24 @@ public class ballMovement : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.name == "top" && other.gameObject.name == "bottomWall")
+        if (other.gameObject.name == "top" || other.gameObject.name == "bottomWall" && body.velocity.y <= 25f)
         {
-            body.velocity += new Vector2(0,0.05f).normalized;
+            body.velocity += new Vector2(0,1f);
         }else if (other.gameObject.name == "middle")
         {
-            body.velocity += new Vector2(0.05f,0.05f).normalized;
-        }else if (other.gameObject.name == "bottom" && other.gameObject.name == "topWall")
+            if (body.velocity.x < 0 )
+            {
+                body.velocity -= new Vector2(1f,1f);
+            }else if (body.velocity.x > 0)
+            {
+                body.velocity += new Vector2(1f,1f);
+            }
+        }else if (other.gameObject.name == "bottom" || other.gameObject.name == "topWall" && body.velocity.y >= -25f)
         {
-            body.velocity += new Vector2(0,-0.05f).normalized;
+            body.velocity += new Vector2(0,-1f);
         }
+
+        print("after" + body.velocity.x + " and " + body.velocity.y);
         gameAudio.clip = ball;
         gameAudio.Play();
     }
---
+++ PongProject/Assets/Scripts/musicManagement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class musicManagement : MonoBehaviour
+{
+    public Text musicOnOff;
+    public static musicManagement instance;
+    private AudioSource gameAudio;
+    // Awake is called before the Start();
+    void Awake()
+    {
+        Singleton();
+        gameAudio = GetComponent<AudioSource>();
+    }
+
+    void Singleton(){
+        if(instance != null){
+            Destroy(gameObject);
+        }if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    public void MusicOnOff()
+    {
+        if(gameAudio.isPlaying == true){
+            gameAudio.Stop();
+            musicOnOff.text = "Music On!";
+        }else if(gameAudio.isPlaying == false)
+        {
+            gameAudio.Play();
+            musicOnOff.text = "Music Off!";
+        }
+    }
+}
